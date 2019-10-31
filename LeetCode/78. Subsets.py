6b810f94189a91ad4041e3196779c3767dd531c2
YuThrones <YuThrones@users.noreply.github.com>
2019-10-31 09:19:12 +0800

    78. Subsets

--- LeetCode/78. Subsets.py
+++ LeetCode/78. Subsets.py
@@ -9,3 +9,16 @@ class Solution:
             result.append([] + sub)
             result.append([nums[0]] + sub)
         return result
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+        if (len(nums) == 0):
+            return result
+        for num in nums:
+            for index in range(len(result)):
+                sub = result[index]
+                new_list = sub[:]
+                new_list.append(num)
+                result.append(new_list)
+        return result
