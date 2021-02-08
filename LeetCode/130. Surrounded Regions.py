10b3742cfe14ef235a3e51717c273257a867c3e0
chenyuhang <496264889@qq.com>
2021-02-08 15:19:05 +0800

    130. Surrounded Regions

--- LeetCode/130. Surrounded Regions.py
+++ LeetCode/130. Surrounded Regions.py
@@ -47,3 +47,48 @@ class Solution:
                     for pos in pos_list:
                         board[pos[0]][pos[1]] = 'X'
                 
+
+# 看了下其他人的答案，更快的解法是，只从最边缘的四条边中的O开始搜索，把与这部分O相连的全部标记为第三种标记，比如N，在全部标记完之后，就可以看到，只要是标记为N的都不需要转换
+# 依然是O的标记的，证明没有跟外围相连，是被包围的，把O转为X，N转为O即可
+# 这种做法还节省了一个用来标记已经访问的set，无论从时间还是空间上都更优
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        # guard clause
+        if not board or not board[0]:
+            return
+        
+        R = len(board)
+        C = len(board[0])
+        
+        # guard clause
+        if R <= 2 or C <= 2:
+            return
+        
+        def dfs(board, r, c, R, C): # dfs when find a "O" change to something diff and recurse
+            if 0 <= r < R and 0 <= c < C and board[r][c] == "O":
+                board[r][c] = "N"
+                dfs(board, r, c+1, R, C)
+                dfs(board, r, c-1, R, C)            
+                dfs(board, r-1, c, R, C)            
+                dfs(board, r+1, c, R, C) 
+                
+                
+        for r in range(R): # loop rows and recurse changing all "O" to "N"s
+            dfs(board, r, 0, R, C)
+            dfs(board, r, C-1, R, C)
+        
+        for c in range(C): # loop cols and recurse changing all "O" to "N"s
+            dfs(board, 0, c, R, C)
+            dfs(board, R-1, c, R, C)
+            
+        # go thru board, change all N's to Os and any remaining O's to X's
+        for r in range(R):
+            for c in range(C): 
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                if board[r][c] == "N":
+                    board[r][c] = "O"
