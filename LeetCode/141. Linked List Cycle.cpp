e32c5aefe07705d8189972301300dd1c95b32ae8
yu <496264889@qq.com>
2021-12-22 22:51:43 +0800

    141. Linked List Cycle

---
+++ LeetCode/141. Linked List Cycle.cpp
@@ -0,0 +1,27 @@
+// 最经典的快慢指针找环了，写熟悉了就行
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (!head) {
+            return false;
+        }
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+        while(fast != NULL && fast->next != NULL) {
+            if (slow == fast) {
+                return true;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return false;
+    }
+};
