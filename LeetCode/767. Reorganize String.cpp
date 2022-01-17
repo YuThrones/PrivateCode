fe851ad1b182180c796146041d735c2a7a02bb03
yu <496264889@qq.com>
2022-01-17 23:10:38 +0800

    767. Reorganize String

---
+++ LeetCode/767. Reorganize String.cpp
@@ -0,0 +1,41 @@
+// 思路比较不明显，先统计词频，然后按照词频入大根堆，每次拿堆顶，并且不与前面一个相同附加到答案，如果堆拿空了还有剩余的词，说明不合法，返回空字符串
+struct cmp {
+    bool operator ()(pair<char, int> a, pair<char, int> b) {
+        return a.second < b.second;
+    }
+};
+
+class Solution {
+public:
+    string reorganizeString(string s) {
+        map<char, int> m;
+        for (int i = 0; i < s.length(); ++i) {
+            m[s[i]] += 1;
+        }
+        priority_queue<pair<char, int>, vector<pair<char, int> >, cmp> pq;
+        for (auto it: m) {
+            pq.push(make_pair(it.first, it.second));
+        }
+        string res = "";
+        pair<char, int> flag = make_pair('\0', 0);
+        pair<char, int> last = flag;
+        pair<char, int> cur = flag;
+        while(!pq.empty()) {
+            cur = pq.top();
+            pq.pop();
+            cur.second -= 1;
+            res += cur.first;
+            if (last != flag) {
+                pq.push(last);
+                last = flag;
+            }
+            if (cur.second > 0) {
+                last = cur;
+            }
+        }
+        if (last != flag) {
+            return "";
+        }
+        return res;
+    }
+};
