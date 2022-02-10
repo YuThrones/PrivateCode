bebcf996c709b358c0130145f6768e4fb024925c
YuThrones <YuThrones@users.noreply.github.com>
2022-02-11 00:01:17 +0800

    138. Copy List with Random Pointer

--- LeetCode/138. Copy List with Random Pointer.cpp
+++ LeetCode/138. Copy List with Random Pointer.cpp
@@ -1,5 +1,6 @@
 // 空间复杂度为O(1)的解法，代价是要遍历三遍，使用原链表节点的random指针暂时存储copy节点，然后使用copy节点的next指针来存储源节点的random
 // 第二次遍历时把指针的random节点都设置好，第三次还原原节点的random指针，并且把copy节点的next指针设置好
+// 如果只是解题，两次遍历配合哈希表解决即可
 
 /*
 // Definition for a Node.
