6b75b2ed07e6a78fedd44aa06bb73bf994e41c54
YuThrones <YuThrones@users.noreply.github.com>
2022-05-16 00:43:04 +0800

    Update 222. Count Complete Tree Nodes.cpp

--- LeetCode/222. Count Complete Tree Nodes.cpp
+++ LeetCode/222. Count Complete Tree Nodes.cpp
@@ -1,6 +1,3 @@
-// 第一反应是最简单的递归统计，但是速度比最佳答案慢
-// 看了下最佳答案，是在递归思路上做了优化，利用完全二叉树的特性，对比左边界和右边界高度，如果一致可以直接2 ** level -1 算出数量
-// 不一致再递归，可以减掉一部分满二叉树的计算
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -15,33 +12,34 @@
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        if (!root) {
-            return 0;
-        }
-        int left_height = leftHeight(root);
-        int right_height = rightHeight(root);
-        if (left_height == right_height) {
-            return (1 << left_height) - 1;
-        }
-        int left_res = countNodes(root->left);
-        int right_res = countNodes(root->right);
-        return left_res + right_res + 1;
+        return cal(root, -1, -1);
     }
     
-    int leftHeight(TreeNode* root) {
+    int cal(TreeNode* root, int l, int r) {
+        if (!root) return 0;
+        
+        if (l == -1) {
+            l = maxSideHeight(root, true);
+        }
+        
+        if (r == -1) {
+            r = maxSideHeight(root, false);
+        }
+        
+        if (l == r) {
+            return pow(2, l) - 1;
+        }
+        else {
+            return cal(root->left, l - 1, -1) + cal(root->right, -1, r - 1) + 1;
+        }
+    }
+    
+    int maxSideHeight(TreeNode* root, bool isLeft) {
         int count = 0;
         while(root) {
             ++count;
-            root = root->left;
-        }
-        return count;
-    }
-    
-    int rightHeight(TreeNode* root) {
-        int count = 0;
-        while(root) {
-            ++count;
-            root = root->right;
+            if (isLeft) root = root->left;
+            else root = root->right;
         }
         return count;
     }
