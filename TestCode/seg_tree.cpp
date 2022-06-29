f1bf7b37f9131373d34b820774a4bc2ead36d115
YuThrones <YuThrones@users.noreply.github.com>
2022-06-29 17:50:49 +0800

    Update seg_tree.cpp

--- TestCode/seg_tree.cpp
+++ TestCode/seg_tree.cpp
@@ -34,6 +34,9 @@ vector<bool> flag; // 代表一个节点是否被赋值过
         if (left <= start && end <= right) {
             return data[node];
         }
+
+        pushDown(start, end, node);
+
         int mid = start + ((end - start) >> 1);
         int res = 0;
         if (mid >= left) res += getNum(left, right, start, mid, node * 2);
@@ -45,15 +48,36 @@ vector<bool> flag; // 代表一个节点是否被赋值过
         // left和right为要增加的区域，start和end为当前增加区域，value为增加的值,node为当前节点
         // 注意如果left和right包括的区分大于1，是每一段都要加上value这个值
         if (left <= start && end <= right) {
+            // 当前区间是目标区间子集，直接修改数据然后返回
             data[node] += (end - start + 1) * value;
             change[node] += value;
             return;
+        }
+
+        int mid = start + ((end - start) >> 1);
+
+        pushDown(start, end, node);
+        if (mid >= left) addNum(left, right, value, start, mid, node * 2);
+        if (mid < right) addNum(left, right, value, mid + 1, end, node * 2 + 1);
+        data[node] = data[node * 2] + data[node * 2 + 1];
+    }
+
+    void pushDown(int start, int end, int node) {
+        if (start == end) return;
+        int mid = (start + ((end - start) >> 1));
+        if (change[node] && end != start) {
+            // 当前节点有修改过，往下更新两个子节点
+            data[node * 2] += (mid - start + 1) * change[node];
+            change[node * 2] += change[node];
+            data[node * 2 + 1] += (end - mid) * change[node];
+            change[node * 2 + 1] += change[node];
+            change[node] = 0;
         }
     }
 
     void print() {
         for (int i = 0; i < data.size(); ++i) {
-            cout << data[i] << " " << change[i] << " " << flag[i] << endl;
+            cout << i << " " << data[i] << " " << change[i] << " " << flag[i] << endl;
         }
     }
 };
@@ -67,11 +91,15 @@ int main() {
     }
     SegTree tree(input);
     tree.print();
+    cout << "add" << endl;
+    tree.addNum(1, 10, 1, 0, count - 1, 1);
+    tree.print();
 
     cout << "check" << endl;
     for (int i = 0; i < count; ++i) {
-        for (int j = i; j < count; ++j) {
-            cout << i << " " << j << " " << tree.getNum(i, j, 0, count - 1, 1) << endl;
-        }
+        // for (int j = i; j < count; ++j) {
+        //     cout << i << " " << j << " " << tree.getNum(i, j, 0, count - 1, 1) << endl;
+        // }
+        cout << i << " " << tree.getNum(i, i, 0, count - 1, 1) << endl;
     }
 }
