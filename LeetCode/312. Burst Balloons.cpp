f17b71edb6e52c93234a82b70ebcca74cc98ab11
YuThrones <YuThrones@users.noreply.github.com>
2022-07-19 00:10:28 +0800

    Update 312. Burst Balloons.cpp

--- LeetCode/312. Burst Balloons.cpp
+++ LeetCode/312. Burst Balloons.cpp
@@ -1,6 +1,6 @@
 // 这道题看过，但是一时忘记了，查了一下，还是使用动态规划解决，需要注意的是切分的方式比较难确定，是从最小的区间往打拓展，每次遍历选定一个气球作为最后打爆的气球
 // 然后求左右区间先打爆的最大收益，迭代顺序必须严格按照小区间到大
-// 看了下最快答案，思路是一样的，但是存储他使用了一维数组来代替二维vector，速度是要强不少，大概5倍差距
+// 看了下最快答案，思路是一样的，但是存储他使用了一维数组来代替二维vector，速度是要强不少，把自己的代码全改一维数组，速度确实提升了很多
 
 #include<vector>
 #include<iostream>
@@ -10,13 +10,15 @@ class Solution {
 public:
     int maxCoins(vector<int>& nums) {
         int len = nums.size();
-        vector<vector<int> > dp(len + 2);
-        for (int i = 0; i < len + 2; ++i) {
-            dp[i].resize(len + 2);
-        }
+        int M = len + 2;
+        int * dp = new int [M * M];
         
-        nums.insert(nums.begin(), 1);
-        nums.push_back(1);
+        int* input = new int[nums.size() + 2];
+        input[0] = 1;
+        memcpy(input + 1, nums.data(), nums.size() * sizeof(int));
+        input[nums.size() + 1] = 1;
+        // nums.insert(nums.begin(), 1);
+        // nums.push_back(1);
         
         for (int step = 0; step <= len; ++step) {
             // step从小到大，保证dp使用时较小的都已经算好
@@ -24,20 +26,20 @@ public:
                 // 根据step找到每一个需要计算的固定长度的区间，如果右边已经超过len不需要进行计算
                 int left = i;
                 int right = i + step;
-                dp[left][right] = 0;
+                dp[left * M + right] = 0;
                 for (int j = i; j <= i + step; ++j) {
                     // 寻找每个气球作为最后一个打爆的
                     // 找到左边[i, j-1]闭区间和右边[j+1, i+step]闭区间作为先打爆的最大值
-                    int sum = nums[left - 1] * nums[j] * nums[right + 1];
+                    int sum = input[left - 1] * input[j] * input[right + 1];
                     int temp = 0;
                     
-                    if (left <= j - 1) sum += dp[left][j - 1];
-                    if (right >= j + 1) sum += dp[j + 1][right];
-                    dp[left][right] = max(sum, dp[left][right]);
+                    if (left <= j - 1) sum += dp[left * M + j - 1];
+                    if (right >= j + 1) sum += dp[(j + 1) * M + right];
+                    dp[left * M + right] = max(sum, dp[left * M +right]);
                 }
             }    
         }
-        return dp[1][len];
+        return dp[1 * M + len];
     }
 };
 
