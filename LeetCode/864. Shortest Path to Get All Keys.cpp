daca958c3d35c6936e1b3af4b071166016f3ecdc
YuThrones <YuThrones@users.noreply.github.com>
2024-06-03 21:14:39 +0800

    Update 864. Shortest Path to Get All Keys.cpp

--- LeetCode/864. Shortest Path to Get All Keys.cpp
+++ LeetCode/864. Shortest Path to Get All Keys.cpp
@@ -96,3 +96,125 @@ int main() {
     int ans = sol.shortestPathAllKeys(grid);
     return 0;
 }
+
+#include <vector>
+#include <string>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <cstring>
+#include <unordered_map>
+using namespace std;
+const int dy[4] = {-1,1,0,0};
+const int dx[4] = {0,0,-1,1};
+const int MAX_QUEUE = 1<<15;
+const int MAX_N = 30;
+const char ROAD = '.';
+typedef unsigned long long ull;
+struct QueueElement{
+    int key,y,x,dist;
+};
+template<typename T>
+class Queue{
+public:
+    Queue() : head(0),tail(0),size(0){}
+    void push(T val){
+        arr[tail] = val;
+        tail = (tail+1)&(MAX_QUEUE-1);
+        ++size;
+    }
+    void pop(){
+        head = (head+1)&(MAX_QUEUE-1);
+        --size;
+    }
+    T front() const {
+        return arr[head];
+    }
+    bool empty() const{
+        return size==0;
+    }
+private:
+    T arr[MAX_QUEUE];
+    int head,tail,size;
+};
+class Solution {
+public:
+    ull visited[30][30];
+    int N,M;
+    inline int getKeyNum(char ch) const{
+        return ch - 'a';
+    }
+    inline int getWallNum(char ch) const{
+        return ch - 'A';
+    }
+    inline bool checkLowerCase(char ch) const{
+        return ch >= 'a' && ch <= 'z';
+    }
+    inline bool checkUpperCase(char ch) const{
+        return ch>='A' && ch<='Z';
+    }
+    auto getStartPos(const vector<string>& grid) const {
+        for(int y=0;y<N;++y){
+            for(int x=0;x<M;++x){
+                if(grid[y][x]=='@'){
+                    return pair<int,int>(y,x);
+                }
+            }
+        }
+        return pair<int,int>(-1,-1);
+    }
+    int calcFullKey(const vector<string>& grid) const {
+        int ret = 0;
+        for(int y=0;y<N;++y){
+            for(int x=0;x<M;++x){
+                if(checkLowerCase(grid[y][x])){
+                    ret|=(1<<getKeyNum(grid[y][x]));                    
+                }
+            }
+        }
+        return ret;
+    }
+    inline bool checkValid(int y,int x){
+        return y>=0 && y<N && x>=0 && x<M;
+    }
+    int shortestPathAllKeys(vector<string>& grid) {
+        memset(visited,0,sizeof(visited));
+        N = grid.size();M = grid[0].size();
+        auto [startY,startX] = getStartPos(grid);
+        grid[startY][startX] = '.';
+        visited[startY][startX] = 1LL;
+        Queue<QueueElement> q;
+        q.push({0,startY,startX,0});
+        const int fullKey = calcFullKey(grid);        
+        while(!q.empty()){
+            auto here = q.front();
+            q.pop();
+            const int key = here.key,y = here.y,x = here.x,dist = here.dist;
+            if(visited[y][x]&(1ull<<fullKey)){
+                return dist;
+            }
+            for(int dir=0;dir<4;++dir){
+                const int cy = y+dy[dir],cx = x+dx[dir];
+                if(checkValid(cy,cx) && (visited[cy][cx]&(1ull<<key))==0){
+                    if(grid[cy][cx]==ROAD){
+                        visited[cy][cx] |= 1ull<<key;
+                        q.push({key,cy,cx,dist+1});
+                    }else if(checkLowerCase(grid[cy][cx])){
+                        const int newKey = key|(1<<getKeyNum(grid[cy][cx]));
+                        visited[cy][cx] |= 1ull<<key;
+                        visited[cy][cx] |= 1ull<<newKey;
+                        q.push({newKey,cy,cx,dist+1});
+                    }else if(checkUpperCase(grid[cy][cx])){
+                        const int wallNum = getWallNum(grid[cy][cx]);
+                        if(key&(1<<wallNum)){
+                            visited[cy][cx] |= 1ull<<key;
+                            q.push({key,cy,cx,dist+1});
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
