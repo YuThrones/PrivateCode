d9933899b1765f7b0d6e789130dbf140b9829347
YuThrones <YuThrones@users.noreply.github.com>
2025-05-06 22:57:44 +0800

    Update 922. Sort Array By Parity II.cpp

--- LeetCode/922. Sort Array By Parity II.cpp
+++ LeetCode/922. Sort Array By Parity II.cpp
@@ -1,22 +1,22 @@
 class Solution {
     public:
         vector<int> sortArrayByParityII(vector<int>& nums) {
-            int i = 0;
-            int j = 1;
-            for (; i < nums.size() && j < nums.size();) {
-                if (nums[i] % 2 == 1) {
-                    swap(nums[i], nums[j]);
-                    j += 2;
+            int n = nums.size();
+            int even = 0, odd = 1;
+    
+            while(even < n && odd < n){
+                if(nums[even] % 2 == 0){
+                    even += 2;
                 }
-                else if (nums[j] % 2 == 0) {
-                    swap(nums[i], nums[j]);
-                    i += 2;
+                else if(nums[odd] % 2 == 1){
+                    odd += 2;
                 }
                 else {
-                    i += 2;
-                    j += 2;
+                    swap(nums[even], nums[odd]);
+                    even += 2;
+                    odd += 2;
                 }
             }
-            return nums;
+            return nums; 
         }
     };
