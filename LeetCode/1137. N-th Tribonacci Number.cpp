ea06398bdfae84d6e73f00e5f1224076025469c9
YuThrones <YuThrones@users.noreply.github.com>
2025-08-27 21:14:03 +0800

    Create 1137. N-th Tribonacci Number.cpp

---
+++ LeetCode/1137. N-th Tribonacci Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n == 0) {return 0;}
+        if (n == 1) {return 1;}
+        if (n == 2) {return 1;}
+        int t0 = 0;
+        int t1 = 1;
+        int t2 = 1;
+        while(n > 2) {
+            int temp = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = temp;
+            --n;
+        }
+        return t2;
+    }
+};
