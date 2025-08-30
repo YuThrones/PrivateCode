83c5dc5c8e75235d03406b96acad92a285a539c4
YuThrones <YuThrones@users.noreply.github.com>
2025-08-30 11:20:30 +0800

    Create 515. Find Largest Value in Each Tree Row.cpp

---
+++ LeetCode/515. Find Largest Value in Each Tree Row.cpp
@@ -0,0 +1,72 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        vector<int>ans;
+        if(root==NULL) return ans;
+        queue<TreeNode*>q;
+        q.push(root);
+
+        while(!q.empty()){
+            int n=q.size();
+            int maxi=INT_MIN;
+            for(int i=0;i<n;i++){
+                TreeNode* node=q.front();
+                q.pop();
+
+                maxi=max(maxi,node->val);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            ans.push_back(maxi);
+        }
+        return ans;
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        vector<int> ans;
+        if (!root) {
+            return ans;
+        }
+        ans.push_back(root->val);
+        vector<int> left = largestValues(root->left);
+        vector<int> right = largestValues(root->right);
+        int n = max(left.size(), right.size());
+        for (int i = 0; i < n; ++i) {
+            int lv = INT_MIN;
+            int rv = INT_MIN;
+            if (left.size() > i) {
+                lv = left[i];
+            }
+            if (right.size() > i) {
+                rv = right[i];
+            }
+            ans.push_back(max(lv, rv));
+        }
+        return ans;
+    }
+};
