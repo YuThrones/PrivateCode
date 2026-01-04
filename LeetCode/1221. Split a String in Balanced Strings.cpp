611f8a99ca24bad676e772de2c1d67f1b06a73d4
YuThrones <YuThrones@users.noreply.github.com>
2026-01-04 21:24:21 +0800

    Create 1221. Split a String in Balanced Strings.cpp

---
+++ LeetCode/1221. Split a String in Balanced Strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int ans = 0;
+        int l = 0;
+        int r = 0;
+        for (auto c : s) {
+            if (c == 'L') {
+                ++l;
+            }
+            else {
+                ++r;
+            }
+            if (l == r) {
+                ++ans;
+                l = 0;
+                r = 0;
+            }
+        }
+        return ans;
+    }
+};
