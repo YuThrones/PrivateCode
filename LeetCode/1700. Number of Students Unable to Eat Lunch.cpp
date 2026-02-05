708a6f00f56452d3045fedc877a55f06680db555
YuThrones <YuThrones@users.noreply.github.com>
2026-02-05 21:35:35 +0800

    Create 1700. Number of Students Unable to Eat Lunch.cpp

---
+++ LeetCode/1700. Number of Students Unable to Eat Lunch.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        vector<int> count(2, 0);
+        for (auto s : students) {
+            ++count[s];
+        }
+        for (int i = 0; i < sandwiches.size(); ++i) {
+            cout << count[0] << count[1] << endl;
+            if (count[sandwiches[i]] <= 0) {
+                return count[0] + count[1];        
+            }
+            --count[sandwiches[i]];
+        }
+        return count[0] + count[1];
+    }
+};
