8da6430994dd452283d9f5a2fe66e15356506c01
YuThrones <YuThrones@users.noreply.github.com>
2026-02-13 10:23:28 +0800

    Update 647. Palindromic Substrings.cpp

--- LeetCode/647. Palindromic Substrings.cpp
+++ LeetCode/647. Palindromic Substrings.cpp
@@ -1,3 +1,34 @@
+// 直接遍历所有长度可以得出结果，但是不够快，看了下最快答案，用的是中心拓展法，省略了很多重复比较
+
+class Solution {
+public:
+    int countSubstrings(string s) {
+        
+        int ans = 0;
+        int n = s.length();
+        for(int i = 0;i<n;i++){
+            int left = i, right = i;
+            int curr = 0;
+            while((left>=0) && (right < n) && s[left] == s[right]){ // palindromes of odd length
+                left--;
+                right++;
+                curr++;
+            }
+
+            left = i, right = i+1;
+            while((left>= 0) && (right < n) && s[left] == s[right]){ // palindromes of even lenght
+                left--;
+                right++;
+                curr++;
+            }
+
+            ans += curr;
+
+        }
+        return ans;
+    }
+};
+
 class Solution {
 public:
     int countSubstrings(string s) {
