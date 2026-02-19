e58eb494ba5798a5e0019bb7a191d290e5cccbde
YuThrones <YuThrones@users.noreply.github.com>
2026-02-19 12:32:50 +0800

    Create 672. Bulb Switcher II.cpp

---
+++ LeetCode/672. Bulb Switcher II.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int flipLights(int n, int presses) {
+        if(presses == 0) {return 1;}
+        if(n == 0) {return 1;}
+        if (n == 1) {
+            if (presses > 0) {return 2;}
+            else {return 1;}
+        }
+        else if(n == 2) {
+            if (presses == 1) {
+                return 3;
+            }
+            else {
+                return 4;
+            }
+        }
+        else {
+            if(presses == 1) {
+                return 4;
+            }
+            else if(presses == 2) {
+                return 7;
+            }
+            else {
+                return 8;
+            }
+        }
+        return 1;
+
+    }
+};
